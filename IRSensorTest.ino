6804c12d4bc4a5bf24ef56050926fd209dcc920c
Keshav Iyengar <keshav.iyengar94@gmail.com>
2016-07-10 13:06:45 -0400

    Finished initial test. Hardware is working.

--- IRSensorTest.ino
+++ IRSensorTest.ino
@@ -2,11 +2,15 @@
  * https://acroname.com/articles/linearizing-sharp-ranger-data
  * To non-linear output analog voltage
  * 
- * 
+ * Hookup:
+ * Yellow to analog pin 2
+ * Red 5V input
+ * Black GND
  */
  
-int v_input;
+double v_input;
 double range;
+const int analogPin = 2;
 const int k = 4.0; // constant for this specific IR
 
 void setup() {
@@ -15,8 +19,8 @@ void setup() {
 }
 
 void loop() {
-  v_input = analogRead(2); // Read from pin 2
+  v_input = analogRead(analogPin); // Read from pin 2
   range = 6787/(v_input - 3) - 4;
-  Serial.println(v_input); // Print raw distance
+  //Serial.println(v_input); // Print raw distance
   Serial.println(range); // Print converted to cm
 }
